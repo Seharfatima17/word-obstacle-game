2f228fcc149fbf3e3c1be3b23598e7e6c1807f23
Digilex <digilex10@gmail.com>
2025-09-18 01:29:26 +0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Homescreen from './screens/HomeScreen';
+import GameScreen from './screens/GameScreen';
+import AdvancedScreen from './screens/AdvancedScreen';
+import BeginnerScreen from './screens/BeginnerScreen';
+import IntermediateScreen from './screens/IntermediateScreen';
+import ExpertScreen from './screens/ExpertScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+          
+        <Stack.Screen name="Home" component={Homescreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name="AdvancedScreen" component={AdvancedScreen} />
+        <Stack.Screen name="BeginnerScreen" component={BeginnerScreen} />
+        <Stack.Screen name="IntermediateScreen" component={IntermediateScreen} />
+        <Stack.Screen name="ExpertScreen" component={ExpertScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
